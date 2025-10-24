860782851cfcee1e347ace3718d59d24eec2c2dc
flavioka1ser-debug <flaviobzr@gmail.com>
2025-10-24 00:30:49 +0000

    Finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,41 +1,61 @@
 #include <stdio.h>
 
 int main() {
+    int tabuleiro[10][10];
     int i, j;
-    // 0 representa água, 3 representa parte de um navio
-   int tabuleiro[10][10] = {
-    {3, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 3, 0, 0, 0, 0, 0, 3, 0},
-    {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
-    {3, 3, 3, 0, 0, 0, 3, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 3, 0, 0, 0}
 
-    };
-
-    printf("Tabuleiro do jogo:\n"); // Imprime o tabuleiro
-    for(int i = 0; i < 10; i++) { // Percorre as linhas
-        for(int j = 0; j < 10; j++) { // Percorre as colunas 
-            printf("%d ", tabuleiro[i][j]); // Imprime o valor na posição [i][j]
+    //1. Preenche o tabuleiro com 0 (água)
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
         }
-        printf("\n");
-    
-   }
-    
-   printf("Posições dos navios (representadas por 3):\n"); // Imprime as posições dos navios
-   for(i = 0; i < 10; i++) { // Percorre as linhas
-       for(j = 0; j < 10; j++) { // Percorre as colunas
-           if(tabuleiro[i][j] == 3) { // Verifica se há um navio na posição [i][j]
-               printf("Navio na posição [%d][%d]\n", i, j); // Imprime a posição do navio
-           }
-       }
-
     }
 
-    
+    //2. Coloca alguns navios no tabuleiro (3 = navios)
+    tabuleiro[2][8] = 3;
+    tabuleiro[2][9] = 3;
+    tabuleiro[2][7] = 3;
+
+    tabuleiro[5][5] = 3;
+    tabuleiro[6][5] = 3;
+    tabuleiro[4][5] = 3;
+
+    tabuleiro[4][2] = 3;
+    tabuleiro[5][3] = 3;
+    tabuleiro[6][4] = 3;
+
+    //3. Habilidade Cone (forma pirâmide)
+    int topo = 1;
+    int centro = 4;
+    for(i = 0; i < 3; i++){ //Altura da Pirâmide
+        for(j = centro - i; j <= centro + i; j++) { //Largura da Pirâmide
+            tabuleiro[topo + i][j] = 5; //Marca a área afetada pela habilidade
+        }
+    }
+
+    //Cruz (5x5)
+    int meio = 7; //Centro da Cruz
+    for(i = meio - 2; i <= meio + 2; i++) {
+        tabuleiro[meio][i] = 5; //Marca a linha vertical da cruz
+        tabuleiro[i][meio] = 5; //Marca a linha horizontal da cruz
+    }
+
+    //Losango (Octaedro 3x3)
+    int centroX = 7, centroY = 2;
+    tabuleiro[centroX - 1][centroY] = 5; //Topo
+    tabuleiro[centroX][centroY - 1] = 5; //Meio Esquerda
+    tabuleiro[centroX][centroY] = 5; //Meio Centro
+    tabuleiro[centroX][centroY + 1] = 5; //Meio Direita
+    tabuleiro[centroX + 1][centroY] = 5; //Base
+
+    //4. Imprime o tabuleiro
+    printf("TABULEIRO DE BATALHA NAVAL:\n");
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
